da70d2ae493486bf0e103462d3e2333b2b030b7e
Koen Gabriëls <koeng@appsaloon.be>
2015-08-27 13:29:35 +0200

    Adds composer.json, composer.lock to include php-oauth-client by fkooman, Updates README.md and adds an initial main plugin file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # wp-oauth-framework
 WordPress OAuth Framework to enable login with popular services
+
+Don't forget to do ```composer install``` when you install this plugin.
+
+This plugin doesn't support any OAuth service out of the box, it is meant as a base on which you can expand using
+WordPress actions and filters.
+
+Credits:
+This plugin makes use of the php-oauth-client by fkooman ( https://github.com/fkooman/php-oauth-client )
---
+++ wp-oauth-framework.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * WordPress OAuth Frame work
+ *
+ * @package   WP_OAuth_Framework
+ * @author    Koen Gabriëls
+ * @link      http://www.appsaloon.be
+ * @copyright 2015 AppSaloon BVBA
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WordPress OAuth Framework
+ * Description:       WordPress OAuth Framework to enable login with popular services
+ * Version:           1.0.0
+ * Author:            Koen Gabriëls
+ * Author URI:        http://www.appsaloon.be
+ */
