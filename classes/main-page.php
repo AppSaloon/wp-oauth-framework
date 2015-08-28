4220f6ee902e7f5c1e0631fb2c4ac048fcc2beb3
Koen Gabriëls <koeng@appsaloon.be>
2015-08-28 13:14:13 +0200

    Moves add_menu page parameters to class constants in main-page.php, adds security check in main-page template

--- classes/main-page.php
+++ classes/main-page.php
@@ -4,16 +4,21 @@ namespace wp_oauth_framework\classes;
 
 class Main_Page {
 
+    const PAGE_TITLE = 'WP OAuth Framework';
+    const MENU_TITLE = 'WP OAuth Framework';
+    const REQUIRED_CAPABILITY = 'manage_options';
+    const MENU_SLUG = 'wpof-main-page';
+
     public function __construct() {
         add_action( 'admin_menu', array( $this, 'add_to_menu' ) );
     }
 
     public function add_to_menu() {
         add_menu_page(
-            'WP OAuth Framework',
-            'WP OAuth Framework',
-            'manage_options',
-            'wpof-main-page',
+            self::PAGE_TITLE,
+            self::MENU_TITLE,
+            self::REQUIRED_CAPABILITY,
+            self::MENU_SLUG,
             array( $this, 'show_page' ),
             '',
             81
--- templates/main-page.php
+++ templates/main-page.php
@@ -1,3 +1,8 @@
+<?php
+    if( ! current_user_can( \wp_oauth_framework\classes\Main_Page::REQUIRED_CAPABILITY ) ) {
+        wp_die( 'Nice try but no sigar' );
+    }
+?>
 <div>
     This plugin allows other plugins to provide login via OAuth services
 </div>
